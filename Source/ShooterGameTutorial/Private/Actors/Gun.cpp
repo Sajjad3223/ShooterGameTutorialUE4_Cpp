189809271268e928c6fc2f6f2378643f3dd1e1f9
Sajjad <m.sajjad1378@gmail.com>
2022-05-28 19:30:12 +0430

    Create Gun Actor

---
+++ Source/ShooterGameTutorial/Private/Actors/Gun.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Gun.h"
+
+// Sets default values
+AGun::AGun()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Root = CreateDefaultSubobject<USceneComponent>(FName("Root"));
+	RootComponent = Root;
+	
+	GunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(FName("Gun"));
+	GunMesh->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void AGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
