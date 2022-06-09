2c31ce9223b93465e25f49d78aab6acfc2973710
Sajjad <m.sajjad1378@gmail.com>
2022-06-09 13:35:42 +0430

    AI MoveTo & SetFocus Functions

--- Source/ShooterGameTutorial/Private/AIs/EnemyController.cpp
+++ Source/ShooterGameTutorial/Private/AIs/EnemyController.cpp
@@ -3,14 +3,24 @@
 
 #include "AIs/EnemyController.h"
 
+#include "Kismet/GameplayStatics.h"
+
 AEnemyController::AEnemyController()
 {
 }
+
 
 void AEnemyController::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp,Warning,TEXT("This pawn is conrolled by EnemyController"));
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this,0);
+
+	MoveToLocation(FVector(1303,700,145));
+
+	SetFocus(PlayerPawn,EAIFocusPriority::Gameplay);
+	
 }
 
 void AEnemyController::Tick(float DeltaTime)
